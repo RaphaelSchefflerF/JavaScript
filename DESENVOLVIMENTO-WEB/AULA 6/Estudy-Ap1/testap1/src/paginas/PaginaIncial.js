6c64fc50a7409dca814d435d347a670d98700d91
RaphaelSchefflerF <raphaelscheffler47@rede.ulbra.br>
2024-01-11 16:24:02 -0300

    up

---
+++ DESENVOLVIMENTO-WEB/AULA 6/Estudy-Ap1/testap1/src/paginas/PaginaIncial.js
@@ -0,0 +1,10 @@
+import React from "react";
+export default function PaginaInicial({}) {
+    return (
+        <div>
+            <h1>Página Inicial</h1>
+            <h2>Seja bem-vindo!</h2>
+            <h3>Escolha uma opção no menu acima.</h3>
+       </div>
+    );
+}
